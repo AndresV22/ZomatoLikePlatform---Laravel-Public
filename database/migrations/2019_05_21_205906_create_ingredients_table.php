cef47d83c175178bc873eac2e385b31bd8fc798d
vagrant <vagrant@homestead>
2019-05-23 20:19:13 +0000

    ingredients, dishes and purchases tables were created. FKs are missing.

---
+++ database/migrations/2019_05_21_205906_create_ingredients_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIngredientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ingredients', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            //$table->foreign('ingredients_id')->references('id')->on('dishes');
+            $table->timestamps();
+            $table->string('name', 20);
+            $table->string('type', 20);
+            $table->string('category', 20);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ingredients');
+    }
+}
---
+++ database/migrations/2019_05_21_211346_create_dishes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDishesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dishes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('name', 20);
+            $table->integer('price');
+            $table->text('description');
+            $table->string('category', 20);
+            $table->integer('discount');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dishes');
+    }
+}
