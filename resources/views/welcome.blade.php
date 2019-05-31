c15f158b0635c313f587182452274c968ba406d7
Maximiliano Orellana <maximiliano.orellana.a@usach.cl>
2019-05-31 13:01:52 -0400

    Test commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Zomato666
                 </div>
 
                 <div class="links">
