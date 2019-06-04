cf9020d4d35024d9af6a07c2f6f9e90239f9bb8c
AndresV22 <andres.alcaino@usach.cl>
2019-06-03 20:26:03 -0400

    Fix: Corregido CommentFactory.php

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -8,6 +8,6 @@ use Faker\Generator as Faker;
 $factory->define(App\Comment::class, function (Faker $faker) {
     return [
       'content' => $faker->realText($maxNbChars = 200, $indexSize = 2),
-      'Value' => $faker->numberBetween($min = 0, $max = 5),
+      'value' => $faker->numberBetween($min = 0, $max = 5),
     ];
 });
