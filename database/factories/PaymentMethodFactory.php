95fb614d1b2a975d5dcd94a610405aae1ed6d3b6
Victor <victor.huanqui@usach.cl>
2019-06-07 14:54:57 -0400

    PaymentMethod fix

--- database/factories/PaymentMethodFactory.php
+++ database/factories/PaymentMethodFactory.php
@@ -8,11 +8,16 @@ use Faker\Provider\de_DE\Payment;
 
 $factory->define(App\PaymentMethod::class, function (Faker $faker) {
     $users_id = DB::table('users')->select('id')->get();
-	$methods = array('Cash', 'Debit Card', 'Credit Card');
+	$methods = $faker->randomElement(array('Cash', 'Debit Card', 'Credit Card'));
+	$bank = $faker->company();
+
+	if($methods == 'Cash'){
+		$bank = 'None';
+	}
 
     return [
         'users_id' => $users_id->random()->id,
-        'type' => $faker->randomElement($methods),
-        'bank' => $faker->company(),
+        'type' => $methods,
+        'bank' => $bank,
     ];
 });
