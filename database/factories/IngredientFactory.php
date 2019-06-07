55737644d6c37ae051cf1002655cc4b9162adce0
Maximiliano Orellana <maximiliano.orellana.a@usach.cl>
2019-06-07 16:24:33 -0400

    Added missing semicolon

--- database/factories/IngredientFactory.php
+++ database/factories/IngredientFactory.php
@@ -9,7 +9,7 @@ $factory->define(App\Ingredient::class, function (Faker $faker) {
 
 	$faker->addProvider(new \FakerRestaurant\Provider\en_US\Restaurant($faker));
 	$categories = array('Oils', 'Meat', 'Fruit', 'Sauce', 'Spice', 'Legume', 'pasta', 'Seaweed', 'Bread');
-	$types = array('Animal', 'Vegetable')
+	$types = array('Animal', 'Vegetable');
 	$dishes_id = DB::table('dishes')->select('id')->get();
 
 	$choice = $faker->numberBetween($min = 0, $max = 3);
