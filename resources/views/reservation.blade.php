13208e47e1f59701cf0e8c02e792c89188b95ed3
Andres <andres.alcaino@usach.cl>
2019-07-24 18:37:23 -0400

    Added reservation blade+css, and guest user to seeder
    Also added alerts if needed in place.blade

--- resources/views/reservation.blade.php
+++ resources/views/reservation.blade.php
@@ -5,15 +5,125 @@
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 <script>$('.select2').select2();</script>
 
+<!--
+Notas:
+	- Falta asociarlo con el controlador.
+	- Falta agregar que una persona "invitado" pueda hacer una reserva. (Posible solucion: un if con el rol de invitado)
+ -->
 
 @extends('layouts.app')
 @section('content')
 <link href="{{ asset('css/main.css') }}" rel="stylesheet">
-@if (Auth::guest() || Auth::user()-> id == 2)
-	<font size="7"><p class="text-center">You cannot reserve.</p></font>
-	<font size="4"><p class="text-center">Please log in to continue.</p></font>
-@else
+<link href="{{ asset('css/reservation.css') }}" rel="stylesheet">
+
+
+<!-- Bootsnipp -->
+<div class="content">
+<div class="container">
+    	<div class="row">
+
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mb30 text-center">
+            <h2>Table reservation for {{$place->name}}</h2>
+        </div>
+      </div>
+	<div class="row">
+
+    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mb30">
+        <div class="tour-booking-form">
+            <form action='reserve' method='post'>
+							@if (Auth::guest())
+								{{csrf_field()}}
+								<input name='user_id' type="hidden" value='0'>
+								<input name='place_name' type="hidden" value='{{$place->name}}'>
+								<input name='place_id' type="hidden" value='{{$place->id}}'>
+								<input name='allow' type="hidden" value='true'>
+							@else
+								{{csrf_field()}}
+								<input name='user_id' type="hidden" value='{{Auth::user()->id}}'>
+								<input name='place_name' type="hidden" value='{{$place->name}}'>
+								<input name='name' type="hidden" value='{{Auth::user()->name}}'>
+								<input name='address' type="hidden" value='{{Auth::user()->email}}'>
+								<input name='place_id' type="hidden" value='{{$place->id}}'>
+								<input name='allow' type="hidden" value='true'>
+							@endif
+                <div class="row">
+
+                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 ">
+                        <h4 class="tour-form-title">To make a reservation, fill the next fields:</h4>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
+                        <div class="form-group">
+                            <label class="control-label" for="date">When do you want to come?</label>
+                            <div class="input-group">
+                                <input name="date" type="text" placeholder="DD/MM/AAAA" class="form-control" required>
+                                <span class="input-group-addon"><i class="fa fa-calendar"></i></span> </div>
+                        </div>
+                    </div>
+										<div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
+                        <div class="form-group">
+                            <label class="control-label required" for="select">Available Tables:</label>
+                            <div class="select">
+                                <select id="select" name="table-code" class="form-control">
+																	@foreach ($tables as $table)
+
+																		@if ($table->taken)
+
+																		@else
+																			<option value="{{$table->code}}"> Table: #{{$table->code}} - Capacity: {{$table->capacity}}</option>
+
+																		@endif
+																	@endforeach
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+										@if (Auth::guest())
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mt30">
+                        <h4 class="tour-form-title">Personal information</h4>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
+                        <div class="form-group">
+                            <label class="control-label" for="name">Name</label>
+                            <input id="name" type="text" placeholder="First Name" class="form-control" required>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
+                        <div class="form-group">
+                            <label class="control-label" for="email"> Email</label>
+                            <input id="email" type="text" placeholder="xxxx@xxxx.xxx" class="form-control" required>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12">
+                        <div class="form-group">
+                            <label class="control-label" for="phone"> Phone</label>
+                            <input id="phone" type="text" placeholder="(222) 222-2222" class="form-control" required>
+                        </div>
+                    </div>
+                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                        <div class="form-group">
+                            <label class="control-label" for="textarea">Something to have in mind?</label>
+                            <textarea class="form-control" id="textarea" name="textarea" rows="4" placeholder="Write Your Requirements"></textarea>
+                        </div>
+                    </div>
+                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                        <button type="submit" name="singlebutton" class="btn btn-primary">send</button>
+                    </div>
+                </div>
+                </form>
+        </div>
+
+    </div>
+	</div>
+
+</div>
+</div>
+
+	<!-- Vista del Charlie
+
 	<div class='container'>
+
+
+
 		<div class='row justify-content-center'>
 			<div class="col-md-6 align-self-center">
 				<p><font size="4"><p class="text-center">Make your reservation at {{$place->name}}</p></font><br>
@@ -48,10 +158,10 @@
 
 									@endif
 								@endforeach
-								
+
           					   </select>
-          				</div> 
-  							
+          				</div>
+
 							<div class="btn-group mr-2" role="group" aria-label="Second group">
 								<button type="submit" class="btn btn-default">Submit Reservation</button>
 							</div>
@@ -61,7 +171,7 @@
 			</div>
 		</div>
 	</div>
+-->
+
 @endif
-
 @endsection
-
